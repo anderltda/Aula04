b84016e27e94e82233cf21ecaacc70a7d9d568ac
Usuário Convidado <Guest@pa06micro03.local>
2019-02-06 23:17:02 -0200

    .

--- Aula04/ViewController.swift
+++ Aula04/ViewController.swift
@@ -4,17 +4,51 @@
 //
 //  Created by Usuário Convidado on 06/02/19.
 //  Copyright © 2019 FIAP. All rights reserved.
-//
+// https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var ivImage: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var lbMessage: UILabel!
+    
+    var orders = [
+        
+        "Saindo um café quentinho",
+        "Preparando um chá",
+        "Saí daqui bebum",
+        "Pimenta no toba é"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        viResult.isHidden = true
+        //ivImage.image = #imageLiteral(resourceName: "Logo")
+        ivImage.image = UIImage(named: "Logo")
     }
 
+    @IBAction func changeLabel(_ sender: UIButton) {
+       lbMessage.text = tfName.text
+       //tfName.resignFirstResponder()
+      
+    }
+
+
+    @IBAction func changeOrder(_ sender: UISegmentedControl) {
+        lbMessage.text = orders[sender.selectedSegmentIndex]
+         view.endEditing(true)
+    }
+    
+    
+    @IBAction func printValue(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    
 
 }
 
